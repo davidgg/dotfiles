eb7b31df66c9ba3853d68b812e08c507fa3b0385
David GG <hola@davidgg.es>
2015-11-13 13:24:19 +0100

    Added log and sample routing

--- init/gulp/gulp-tasks/server/express-server.js
+++ init/gulp/gulp-tasks/server/express-server.js
@@ -7,15 +7,19 @@
   var express = require('express');
   var serveIndex = require('serve-index');
   var app = express();
-
+  
+  console.log('Starting server...');
+  
+  app.use(serveIndex('./'), log_server);
   var log_server =  function(req, res, next) {
     console.log(req.originalUrl);
     next();
   };
-
-  //Run a static express server
-  //app.use(serveIndex('./'), log_server);
+  
   app.use(express.static('./'));
+  // routing
+  app.use('/content/images', express.static('./img'));
+  
   app.listen(local_port);
 
   return app;
